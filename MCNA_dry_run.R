96a4235275b5c0d41c89d8259d0567619abce6b0
Yann Say <yann.say@hotmail.com>
2019-08-05 10:06:44 +0300

    respons1-2-3

--- MCNA_dry_run.R
+++ MCNA_dry_run.R
@@ -10,6 +10,8 @@ library(composr) # horziontal operations
 source("functions/to_alphanumeric_lowercase.R") # function to standardise column headers (like check.names)
 source("functions/analysisplan_factory.R")  # generate analysis plans
 
+library(lubridate)
+
 # load questionnaire inputs
 questions <- read.csv("input/questionnaire/SOM_JMCNA_HH_Tool_FIN_2019_settlements_questions.csv", 
                       stringsAsFactors=F, check.names=F)
@@ -40,74 +42,99 @@ response <- response %>%
 # 
 # response_filtered_w_clusterids <- response_filtered %>% 
 #   mutate(strata = paste0(lookup_table$district[match(cluster_location_id,cluster_lookup_table$new_ID)],type_hh))
-
-
+# 
+# 
 # horizontal operations / recoding
 # 
-# r <- response_filtered_w_clusterids %>%
-#   new_recoding(source=how_much_debt, target=hh_with_debt_value) %>% 
-#   recode_to(0.25,where.num.larger.equal = 505000,otherwise.to=0) %>% 
+# source("source/Pre-existing vulnerability indicators.R")
+# r <- response %>%
+#   new_recoding(source=how_much_debt, target=hh_with_debt_value) %>%
+#   recode_to(0.25,where.num.larger.equal = 505000,otherwise.to=0) %>%
 # 
-#   new_recoding(target=hh_unemployed) %>% 
-#   recode_to(0 ,where=!(is.na(response_filtered$work) | is.na(response_filtered$actively_seek_work))) %>% 
-#   recode_to(0.5,where=(work == "no") & (actively_seek_work == "yes")) %>% 
+#   new_recoding(target=hh_unemployed) %>%
+#   recode_to(0 ,where=!(is.na(response_filtered$work) | is.na(response_filtered$actively_seek_work))) %>%
+#   recode_to(0.5,where=(work == "no") & (actively_seek_work == "yes")) %>%
 # 
-#   new_recoding(source=reasons_for_debt, target=hh_unable_basic_needs) %>% 
-#   recode_to(0.25, where.selected.any = c("health","food","education","basic_hh_expenditure"), otherwise.to=0) %>% 
-#   
+#   new_recoding(source=reasons_for_debt, target=hh_unable_basic_needs) %>%
+#   recode_to(0.25, where.selected.any = c("health","food","education","basic_hh_expenditure"), otherwise.to=0) %>%
+# 
 #   end_recoding
-  
+#   
 # r <- r %>% mutate(score_livelihoods = hh_with_debt_value+hh_unemployed+hh_unable_basic_needs)
 
 # vertical operations / aggregation
 
+##to be removed when complete dataset and sampling frame
+samplingframe <- samplingframe %>% dplyr::filter(strata %in% response$strata)
+response <- response %>% dplyr::select(-c(`_id`, `__version__`, `_uuid`, `_submission_time`, `_index`)) %>%
+  dplyr::filter(strata %in% samplingframe$strata)
+
 # make analysisplan including all questions as dependent variable by HH type, repeated for each governorate:
 analysisplan<-make_analysisplan_all_vars(response,
-                                         questionnaire
-                                         ,independent.variable = "yes_no_host",
+                                         questionnaire,
+                                         independent.variable = "yes_no_host",
                                          repeat.for.variable = "region",
-                                         hypothesis.type = "group_difference" 
-                                         )
+                                         hypothesis.type = "group_difference"
+                                         ) %>%
+  filter(dependent.variable == "time_market")
 
-strata_weight_fun <- map_to_weighting(sampling.frame = samplingframe,
-                 sampling.frame.population.column = "Population",
-                 sampling.frame.stratum.column = "strata",
-                 data.stratum.column = "strata")
+analysisplan2 <- data.frame(
+  research.question = "",
+  sub.research.question = "",
+  # hypothesis = "hp 1",
+  repeat.for.variable = "region",
+  independent.variable = "yes_no_host",
+  independent.variable.type = c("categorical"),
+  # dependent.variable = c("HHEX", "access_to_market_score", "time_market"),
+  # hypothesis.type = c("direct_reporting", "group_difference", "group_difference"),
+  # independent.variable.type = c("categorical"),
+  # dependent.variable.type = c("numerical", "categorical", "categorical"))
+  dependent.variable = c("time_market"),
+  dependent.variable.type = c("categorical"),
+  hypothesis.type = c("group_difference"))
+
+analysisplan3 <- data.frame(
+  research.question = "RQ: not specified (automatic analysisplan)",
+  sub.research.question = "sub RQ not specified (automatic analysisplan)",
+  # hypothesis = "hp 1",
+  repeat.for.variable = "region",
+  independent.variable = "yes_no_host",
+  independent.variable.type = c("categorical"),
+  # dependent.variable = c("HHEX", "access_to_market_score", "time_market"),
+  # hypothesis.type = c("direct_reporting", "group_difference", "group_difference"),
+  # independent.variable.type = c("categorical"),
+  # dependent.variable.type = c("numerical", "categorical", "categorical"))
+  dependent.variable = c("time_market"),
+  dependent.variable.type = c("categorical"),
+  hypothesis.type = c("group_difference"))
 
 response$general_weights <- strata_weight_fun(response)
 
-response$cluster_id <- paste(response$settlement,response$yes_no_idp,sep = "_")
+# response$cluster_id <- paste(response$settlement,response$yes_no_idp,sep = "_")
 
-results <- from_analysisplan_map_to_output(response, analysisplan = analysisplan,
-                                          weighting = strata_weight_fun,
-                                          cluster_variable_name = "cluster_id",
-                                          questionnaire)
+results <- from_analysisplan_map_to_output(response, 
+                                           analysisplan = analysisplan,
+                                           weighting = strata_weight_fun,
+                                           cluster_variable_name = "settlement",
+                                           questionnaire)
 
+results2 <- from_analysisplan_map_to_output(response, 
+                                            analysisplan = analysisplan2,
+                                            weighting = strata_weight_fun,
+                                            cluster_variable_name = "settlement",
+                                            questionnaire)
 
-# result_labeled <- result$results %>% lapply(map_to_labeled,questionnaire)
-
-# # exporting only small part of results for speed during testing:
-# subset_of_results<- rep(FALSE,length(results$results))
-# subset_of_results[500:700]<-TRUE
-# some_results<-hypegrammaR:::results_subset(results,logical = subset_of_results)
-some_results<-results
-# not sure if this function should be "user facing" or have some wrappers (@Bouke thoughts?)
-# essentially it handles all the looping over different column values as hierarchies.
-# then each result is visualised by a function passed here that decides how to render each individual result
-# see ?hypegrammaR:::map_to_generic_hierarchical_html
-hypegrammaR:::map_to_generic_hierarchical_html(some_results,
-                                               render_result_with = hypegrammaR:::from_result_map_to_md_table,
-                                               by_analysisplan_columns = c("dependent.var","repeat.var.value"),
-                                               by_prefix =  c("",""),
-                                               level = 2,
-                                               questionnaire = questionnaire,
-                                               label_varnames = TRUE,
-                                               dir = "./output",
-                                               filename = "summary_by_dependent_var_then_by_repeat_var.html"
-                                               )
-browseURL("summary_by_dependent_var_then_by_repeat_var.html")
+results3 <- from_analysisplan_map_to_output(response, 
+                                            analysisplan = analysisplan3,
+                                            weighting = strata_weight_fun,
+                                            cluster_variable_name = "settlement",
+                                            questionnaire)
 
 
-# not sure this is working correctly.. next on agenda (:
-# big_table <- hypegrammaR:::map_to_datamerge(results$results, questionnaire = questionnaire, rows = "repeat.var.value")
+analysisplan
+analysisplan2
+analysisplan3
 
+results$analysisplan
+results2$analysisplan
+results3$analysisplan
