b621617e8b156138000587a316182c88ec379749
Yann Say <yann.say@hotmail.com>
2019-08-21 16:39:37 +0300

    removed all loading

--- MCNA_dry_run.R
+++ MCNA_dry_run.R
@@ -20,71 +20,68 @@ questions <- read.csv("input/questionnaire/SOM_JMCNA_HH_Tool_FIN_2019_settlement
 choices <- read.csv("input/questionnaire/SOM_JMCNA_HH_Tool_FIN_2019_settlements_choices.csv", 
                     stringsAsFactors=F, check.names=F)
 
-
-# read data
-# response <- read.csv("input/data/REACH_JMCNA_DATA_CLEANING_AMRAN.csv",
-#                       stringsAsFactors = F, check.names = F)
-response <- readRDS("input/data/data.RDS")
-
-# response <- response[1:500,] #shorter version for working on variables
-# response$arrived_current <- as.integer(response$arrived_current) #correctig dates value
-
-names(response)<-to_alphanumeric_lowercase(names(response))
-
-
-response_as_read <- response
-
-
-response <- response %>%
-  select(-ends_with("note")) %>%
-  select(-starts_with("sv_")) %>%
-  select(-starts_with("_"), "_uuid") %>%
-  select(- c(start, end, deviceid, consensus))
-
-
-questionnaire <- load_questionnaire(response,questions,choices)
-
 #load sampling frame
 source("source/sampling.R")
 # load the sampling frame into an object called samplingframe
 # load the cluster sampling frame into an object called clustersamplingframe
-
-response <- response %>% 
-  left_join(select(clustersamplingframe, "P_CODE", "strata"), by = c("settlement" = "P_CODE"))
-response %>% filter(is.na(strata)) %>% nrow()
-##to be removed when complete dataset and sampling frame
-response <- response %>%
-  filter(!is.na(strata))
-response %>% filter(is.na(strata)) %>% nrow()
-
-samplingframe <- samplingframe %>% dplyr::filter(strata %in% response$strata)
-response <- response %>% 
-  dplyr::filter(strata %in% samplingframe$strata)
-
-##to be removed when the data analysis problem is solved
-response_no_added_variables <- response
-######
-
-##source("unicefledd thinkgs)
-
-# add cluster ids
-
-# horizontal operations / recoding
+# read data
+##################################   it takes 25 minutes to compute all variables, dont run this all the time
+# response <- readRDS("input/data/data.RDS")
+#  
+# names(response)<-to_alphanumeric_lowercase(names(response))
 # 
-source("source/composite variables/01-horizontal_general.R")
-source("source/composite variables/02-preexisting.R")
-source("source/composite variables/03-education.R")
-source("source/composite variables/04-nutrition.R")
-source("source/composite variables/05-health.R")
-source("source/composite variables/06-shelter_nfi.R")
-source("source/composite variables/07-fsl.R")
-source("source/composite variables/08-wash.R")
-source("source/composite variables/09-protection.R")
-source("source/composite variables/10-mcsi.R")
-source("source/composite variables/11-item_repo.R")
-source("source/composite variables/12-final.R")
+# 
+# response_as_read <- response
+# 
+# 
+# response <- response %>%
+#   select(-ends_with("note")) %>%
+#   select(-starts_with("sv_")) %>%
+#   select(-starts_with("_"), "_uuid") %>%
+#   select(- c(start, end, deviceid, consensus))
+# 
+# 
+# questionnaire <- load_questionnaire(response,questions,choices)
+# 
 
-
+# 
+# response <- response %>%
+#   left_join(select(clustersamplingframe, "P_CODE", "strata"), by = c("settlement" = "P_CODE"))
+# response %>% filter(is.na(strata)) %>% nrow()
+# ##to be removed when complete dataset and sampling frame
+# response <- response %>%
+#   filter(!is.na(strata))
+# response %>% filter(is.na(strata)) %>% nrow()
+# 
+# samplingframe <- samplingframe %>% dplyr::filter(strata %in% response$strata)
+# response <- response %>%
+#   dplyr::filter(strata %in% samplingframe$strata)
+# 
+# ##to be removed when the data analysis problem is solved
+# response_no_added_variables <- response
+# ######
+# 
+# ##source("unicefledd thinkgs)
+# 
+# # add cluster ids
+# 
+# # horizontal operations / recoding
+# #
+# source("source/composite variables/01-horizontal_general.R")
+# source("source/composite variables/02-preexisting.R")
+# source("source/composite variables/03-education.R")
+# source("source/composite variables/04-nutrition.R")
+# source("source/composite variables/05-health.R")
+# source("source/composite variables/06-shelter_nfi.R")
+# source("source/composite variables/07-fsl.R")
+# source("source/composite variables/08-wash.R")
+# source("source/composite variables/09-protection.R")
+# source("source/composite variables/10-mcsi.R")
+# source("source/composite variables/11-item_repo.R")
+# source("source/composite variables/12-final.R")
+# response %>% saveRDS("input/data/02-data_final_scoring21082019.RDS")
+################################## END--  it takes 25 minutes to compute all variables, dont run this all the time
+response <- readRDS("input/data/02-data_final_scoring21082019.RDS")
 
 # make analysisplan including all questions as dependent variable by HH type, repeated for each governorate:
 
@@ -153,7 +150,12 @@ hypegrammaR:::map_to_generic_hierarchical_html(results_hc_idp,
                                                dir = "./output",
                                                filename = "hc_idp_test.html")
 
+
 browseURL("hc_idp_test.html")
+
+big_table <- results_hc_idp$results %>% lapply(function(x) x[["summary.statistic"]]) %>% do.call(rbind, .)
+write.csv(big_table, "output/big_table.csv", row.names = F)
+# response %>% write.csv("output/dataset_with_var.csv", row.names = F)
 # 
 # some_results_refugee_returnee <- results_refugee_returnee[1:200]
 # 
